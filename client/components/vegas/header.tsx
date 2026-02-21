57c1ac21ad74b6b0f054278e920fe8cb97755646
Jesse Arias <jesse.ariaslirio@gmail.com>
2026-02-20 17:38:43 -0800

    Fixed small errors

--- client/components/vegas/header.tsx
+++ client/components/vegas/header.tsx
@@ -14,7 +14,7 @@ export function VegasHeader() {
   const pathname = usePathname();
 
   return (
-    <header className="sticky top-0 z-50 border-b-4 border-yellow-500 bg-gradient-to-r from-red-900 via-black to-red-900">
+    <header className="sticky top-0 z-50 border-b-4 border-yellow-500 bg-linear-to-r from-red-900 via-black to-red-900">
       <div className="mx-auto flex h-16 max-w-6xl items-center justify-between px-4">
         <Link href="/" className="flex items-center gap-2 text-white">
           <span className="text-2xl font-bold">
